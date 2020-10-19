86b9d9f53290dc97af36d33697d257e789541637
karishmakrishnan <karishmakrishnan321@gmail.com>
2020-10-19 12:16:14 +0530

    created a singleton class , TankController inherit from that and added basic tank movement

---
+++ Assets/script/MonoSingleton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T: MonoSingleton<T>{
+private static T instance;
+public static T Instance{
+	get{
+		return instance;
+	}
+}
+private void Awake(){
+	if(instance== null){
+		instance=(T)this;
+	}
+	else{
+		Destroy(this);
+	}
+}
+}
